2e329674ca85b7b2b7d7a8a5aaf4bb376741a181
Shlok Kapoor <shlokkapoor@Shloks-MacBook-Pro.local>
2017-06-01 11:48:27 +0530

    Initial Commit

---
+++ Animation Done/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animation Done
+//
+//  Created by Shlok Kapoor on 01/06/17.
+//  Copyright © 2017 App Gali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
